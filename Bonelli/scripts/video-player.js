e774939e1ea2cdd771cdb6b640190b035109eec4
caiolins12 <caiolins12@gmail.com>
2025-05-13 10:54:14 -0300

    Fix v0.5

--- Bonelli/scripts/video-player.js
+++
@@ -1,8 +0,0 @@
-function playVideo(container) {
-    const videoWrapper = container.parentElement;
-    const video = videoWrapper.querySelector(".video-real");
-
-    container.style.display = "none"; // esconde thumbnail
-    video.style.display = "block";    // mostra vídeo
-    video.play();                     // inicia reprodução
-}
